aee9782991ecb6bcb782695f3c85fe4ddeacc8e8
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-05-07 17:11:42 -0400

    added functionality to combine multiple medications and their results

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,54 @@
 import { useState } from 'react'
-
 import './App.css'
 
 function App() {
-  const [medication, setMedication] = useState('')
+  const [medications, setMedications] = useState([''])
   const [results, setResults] = useState([])
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    setMedication(medication)
-    const res = await fetch (`https://api.fda.gov/drug/event.json?search=patient.drug.openfda.substance_name:${medication}&count=patient.reaction.reactionmeddrapt.exact`)
-    const data = await res.json()
-    const results = data.results;
-    setResults(results)
-    console.log('data', data)
-    console.log('results', results)
+    const searchPromises = medications.map(medication => (
+      fetch(`https://api.fda.gov/drug/event.json?search=patient.drug.openfda.substance_name:${medication}&count=patient.reaction.reactionmeddrapt.exact`)
+        .then(res => res.json())
+    ))
+    const results = await Promise.all(searchPromises)
+    setResults(results.flatMap(result => result.results))
   }
-  const sum = results ? results.reduce((acc, cur) => acc + cur.count, 0) : 0;
 
-  
-  
-  
+  const handleAddMedication = () => {
+    setMedications([...medications, ''])
+  }
+
+  const handleMedicationChange = (index, value) => {
+    setMedications(prevState => {
+      const newState = [...prevState]
+      newState[index] = value
+      return newState
+    })
+  }
+
+  const sum = results.reduce((acc, cur) => acc + cur.count, 0)
+
   return (
     <section className='bg-blue-950 text-white'>
       <form onSubmit={handleSubmit} >
-        <div className='py-4'>
-          <label className=''>Medication </label>
-          <input type='text' className='rounded-lg text-blue-950' onChange={(e) => setMedication(e.target.value)}/> 
-        </div>
+        {medications.map((medication, index) => (
+          <div key={index} className='py-4'>
+            <label className=''>Medication {index + 1} </label>
+            <input type='text' className='rounded-lg text-blue-950' value={medication} onChange={(e) => handleMedicationChange(index, e.target.value)}/> 
+          </div>
+        ))}
         <div className="flex flex-row gap-4 py-2 justify-center text-blue-950">
-          <button className='border-2 border-solid border-white w-auto p-1 rounded-lg bg-blue-100 px-2'>Add Medication</button>
+          <button type='button' className='border-2 border-solid border-white w-auto p-1 rounded-lg bg-blue-100 px-2' onClick={handleAddMedication}>Add Medication</button>
           <button type='submit' className='border-2 border-solid border-white w-auto p-1 rounded-lg bg-blue-100 px-2'>Submit</button>
         </div>
       </form>
       <div className="container grid grid-cols">
         <ul>
-          {results ? results.map((result) => {
+          {results.length > 0 ? results.map((result, index) => {
             const percent = ((result.count / sum)*100).toFixed(2);
             return (
-            <li key={result.term}>
+            <li key={index}>
               <p>{result.term} ({percent}%)</p>
             </li>
             )}
@@ -48,8 +58,8 @@ function App() {
         </ul>
       </div>
       <div className="grid grid-cols-1 justify-center place-content-center justify-items-center py-4">
-          <div className="font-bold">Total Count: {sum} </div>
-        </div>
+        <div className="font-bold">Total Count: {sum} </div>
+      </div>
     </section>
   )
 }
