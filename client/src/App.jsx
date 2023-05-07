71e502ec205bf7c1937bf1016525dda82ecfbb22
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-05-07 15:11:53 -0400

    added retreiving of one med

---
+++ client/src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [medication, setMedication] = useState('')
+  const [results, setResults] = useState([])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setMedication(medication)
+    const res = await fetch (`https://api.fda.gov/drug/event.json?search=patient.drug.openfda.substance_name:${medication}&count=patient.reaction.reactionmeddrapt.exact`)
+    const data = await res.json()
+    setResults(data.results)
+    console.log('data', data)
+    console.log('results', results)
+  }
+
+  //TODO: take sum as denominator and assign each results.count as percentages of total
+  const findPercent = (arr, percent) => {
+    const sum = results ? results.reduce((acc, cur) => acc + cur.count, 0) : 0;
+    const part = []
+    let curr = 0;
+    for ( let i = 0; i < results.length; i ++) {
+      curr + results.count[i];
+      
+    }
+  }
+  return (
+    <section className='bg-blue-950 text-white'>
+      <form onSubmit={handleSubmit} >
+        <div className='py-4'>
+          <label className=''>Medication </label>
+          <input type='text' className='rounded-lg text-blue-950' onChange={(e) => setMedication(e.target.value)}/> 
+        </div>
+        <div className="flex flex-row gap-4 py-2 justify-center text-blue-950">
+          <button className='border-2 border-solid border-white w-auto p-1 rounded-lg bg-blue-100 px-2'>Add Medication</button>
+          <button type='submit' className='border-2 border-solid border-white w-auto p-1 rounded-lg bg-blue-100 px-2'>Submit</button>
+        </div>
+      </form>
+      <div className="container grid grid-cols">
+        <ul>
+          {results ? results.map((result) => (
+            <li key={result.term}>
+              <p>{result.term}{result.count}</p>
+            </li>
+          )) : 
+          (<p>No results to display</p>)
+          }
+        </ul>
+      </div>
+      <div className="grid grid-cols-1 justify-center place-content-center justify-items-center py-4">
+          <div className="font-bold">Total Count: {sum} </div>
+        </div>
+    </section>
+  )
+}
+
+export default App
