abe74c4e424d8d3d72647d5912afe00c9519ea7c
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-05-07 17:37:24 -0400

    percentages update as total, need to prevent repetitive results

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,59 +4,97 @@ import './App.css'
 function App() {
   const [medications, setMedications] = useState([''])
   const [results, setResults] = useState([])
+  const [loading, setLoading] = useState(false)
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    const searchPromises = medications.map(medication => (
-      fetch(`https://api.fda.gov/drug/event.json?search=patient.drug.openfda.substance_name:${medication}&count=patient.reaction.reactionmeddrapt.exact`)
-        .then(res => res.json())
-    ))
-    const results = await Promise.all(searchPromises)
-    setResults(results.flatMap(result => result.results))
+  const handleMedicationChange = (e, index) => {
+    const newMedications = [...medications]
+    newMedications[index] = e.target.value
+    setMedications(newMedications)
   }
 
   const handleAddMedication = () => {
     setMedications([...medications, ''])
   }
 
-  const handleMedicationChange = (index, value) => {
-    setMedications(prevState => {
-      const newState = [...prevState]
-      newState[index] = value
-      return newState
-    })
+  const handleRemoveMedication = (index) => {
+    const newMedications = [...medications]
+    newMedications.splice(index, 1)
+    setMedications(newMedications)
   }
 
-  const sum = results.reduce((acc, cur) => acc + cur.count, 0)
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setLoading(true)
+    const results = []
+    for (const medication of medications) {
+      const res = await fetch(`https://api.fda.gov/drug/event.json?search=patient.drug.openfda.substance_name:${medication}&count=patient.reaction.reactionmeddrapt.exact`)
+      const data = await res.json()
+      const countResults = data.results
+      const totalCount = countResults.reduce((acc, cur) => acc + cur.count, 0)
+      const percentageResults = countResults.map((result) => {
+        return {
+          term: result.term,
+          count: result.count,
+          percentage: (result.count / totalCount) * 100,
+        }
+      })
+      results.push(...percentageResults)
+    }
+    setLoading(false)
+    setResults(orderByPercentage(results))
+  }
+
+  const orderByPercentage = (results) => {
+    return results.sort((a, b) => b.percentage - a.percentage)
+  }
+
+  const sum = results ? results.reduce((acc, cur) => acc + cur.count, 0) : 0
 
   return (
-    <section className='bg-blue-950 text-white'>
-      <form onSubmit={handleSubmit} >
-        {medications.map((medication, index) => (
-          <div key={index} className='py-4'>
-            <label className=''>Medication {index + 1} </label>
-            <input type='text' className='rounded-lg text-blue-950' value={medication} onChange={(e) => handleMedicationChange(index, e.target.value)}/> 
-          </div>
-        ))}
+    <section className="bg-blue-950 text-white">
+      <form onSubmit={handleSubmit}>
+        <div className="py-4">
+          {medications.map((medication, index) => (
+            <div key={index}>
+              <label>Medication #{index + 1} </label>
+              <input
+                type="text"
+                className="rounded-lg text-blue-950"
+                value={medication}
+                onChange={(e) => handleMedicationChange(e, index)}
+              />
+              <button type="button" onClick={() => handleRemoveMedication(index)} className='text-blue-950 ml-4 border-2 border-solid border-white w-auto p-1 rounded-lg bg-blue-100 px-2'>
+                Remove
+              </button>
+            </div>
+          ))}
+        </div>
         <div className="flex flex-row gap-4 py-2 justify-center text-blue-950">
-          <button type='button' className='border-2 border-solid border-white w-auto p-1 rounded-lg bg-blue-100 px-2' onClick={handleAddMedication}>Add Medication</button>
-          <button type='submit' className='border-2 border-solid border-white w-auto p-1 rounded-lg bg-blue-100 px-2'>Submit</button>
+          <button type="button" onClick={handleAddMedication} className='border-2 border-solid border-white w-auto p-1 rounded-lg bg-blue-100 px-2'>
+            Add Medication
+          </button>
+          <button type="submit" className="border-2 border-solid border-white w-auto p-1 rounded-lg bg-blue-100 px-2">
+            {loading ? 'Loading...' : 'Submit'}
+          </button>
         </div>
       </form>
       <div className="container grid grid-cols">
         <ul>
-          {results.length > 0 ? results.map((result, index) => {
-            const percent = ((result.count / sum)*100).toFixed(2);
-            return (
-            <li key={index}>
-              <p>{result.term} ({percent}%)</p>
-            </li>
-            )}
-            ) : 
-          (<p>No results to display</p>)
-          }
+          {results ? (
+            results.map((result, index) => {
+              return (
+                <li key={index}>
+                  <p>
+                    {result.term} ({result.percentage.toFixed(2)}%)
+                  </p>
+                </li>
+              )
+            })
+          ) : (
+            <p>No results to display</p>
+          )}
         </ul>
-      </div>
+        </div>
       <div className="grid grid-cols-1 justify-center place-content-center justify-items-center py-4">
         <div className="font-bold">Total Count: {sum} </div>
       </div>
