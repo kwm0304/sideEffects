150b4de1a25df8def098355f6163336dcc9fcf67
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-05-08 13:34:57 -0400

    added pieChart display for onSubmit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
+//TODO: Add error handling if fetch fails
+//If fetch fails, submit btn gets stuck on loading... 
+//Implement pie chart, https://www.npmjs.com/package/react-minimal-pie-chart
 import { useState } from 'react'
 import './App.css'
+import { PieChart } from 'react-minimal-pie-chart'
 
 function App() {
   const [medications, setMedications] = useState([''])
   const [results, setResults] = useState([])
   const [loading, setLoading] = useState(false)
+  const [data, setData] = useState([])
 
   const handleMedicationChange = (e, index) => {
     const newMedications = [...medications]
@@ -31,40 +36,63 @@ function App() {
       const res = await fetch(`https://api.fda.gov/drug/event.json?search=patient.drug.openfda.substance_name:${medication}&count=patient.reaction.reactionmeddrapt.exact`)
       const data = await res.json()
       const countResults = data.results
-      const totalCount = countResults.reduce((acc, cur) => acc + cur.count, 0)
+      const totalCount = countResults.reduce((acc, cur) => acc + cur.count, 0) 
+      
       const percentageResults = countResults.map((result) => {
         return {
-          term: result.term,
-          count: result.count,
-          percentage: (result.count / totalCount) * 100,
+          title: result.term,
+          value: result.count,
+          percentage: (result.count / totalCount) * 100
         }
       })
       results.push(...percentageResults)
+      console.log('DATA', data)
     }
     setLoading(false)
-    const uniqueResults = Array.from(new Set(results.map(result => result.term))).map(term => {
-      const filteredResults = results.filter(result => result.term === term)
-      const totalCount = filteredResults.reduce((acc, cur) => acc + cur.count, 0)
-      const percentage = (totalCount / results.reduce((acc, cur) => acc + cur.count, 0)) * 100
+    const uniqueResults = Array.from(new Set(results.map(result => result.title))).map(title => {
+      const filteredResults = results.filter(result => result.title === title)
+      console.log('filteredResults', filteredResults)
+      console.log('typeOf', typeof filteredResults)
+      
+      const randomColor = Math.floor(Math.random()*16777215).toString(16);
+      const totalCount = filteredResults.reduce((acc, cur) => acc + cur.value, 0)
+      const percentage = totalCount !== 0 ? (totalCount / results.reduce((acc, cur) => acc + cur.value, 0)) * 100 : 0;
+      console.log('FRVALUE', filteredResults[0].value)
+      console.log('totalCount', totalCount)
+      console.log('percentageType',typeof percentage)
       return {
-        term: term,
-        count: totalCount,
-        percentage: percentage
+        title: title,
+        color:'#' + randomColor,
+        value: totalCount,
+        toolTip: title
       }
-    })
+    }).slice(0, 20)
     setResults(orderByPercentage(uniqueResults))
+    setData(uniqueResults)
+    
+    console.log('uniqueResults', uniqueResults)
   }
 
+  
   const orderByPercentage = (results) => {
     return results.sort((a, b) => b.count - a.count)
   }
 
-  const sum = results ? results.reduce((acc, cur) => acc + cur.count, 0) : 0
+  const sum = results ? results.reduce((acc, cur) => acc + cur.value, 0) : 0
+  console.log(sum)
+
+  const defaultLabelStyle = {
+    fontSize: '1.5px',
+    fontFamily: 'sans-serif',
+    fill: 'white'
+    
+  }
   return (
     <section className="bg-blue-950 text-white">
       <h1 className='text-4xl font-semibold pt-6'>Side <span className='font-bold text-4xl text-amber-600'>FX</span></h1>
       <h3 className='text-white pt-6 pb-12 italic'>Enter the exact name of your medication in the box below. <br/>
-      You can add more by clicking 'Add Medication' and can remove them by clicking the trash button.
+      You can add more by clicking 'Add Medication' and can remove them by clicking the trash button.<br/>
+      Results limited to top 20 for readability.
       </h3>
       <form onSubmit={handleSubmit}>
         <div className="py-4">
@@ -92,24 +120,23 @@ function App() {
           </button>
         </div>
       </form>
-      <div className="container grid grid-cols">
-        <ul>
-          {results ? (
-            results.map((result, index) => {
-              return (
-                <li key={index}>
-                  <p>
-                    <span className='text-amber-500 font-semibold'>{result.term}</span> ({result.percentage.toFixed(2)}%)
-                  </p>
-                </li>
-              )
-            })
-          ) : (
-            <p>No results to display. Check the spelling of medication or look up exact name. (e.g. Tylenol doesn't work but acetaminophen will)</p>
-          )}
-        </ul>
+      <div>
+        {data ? 
+        <PieChart 
+        data={data} 
+        label={({ dataEntry }) => `${Math.round(dataEntry.percentage)} %  ${dataEntry.title}`} 
+        labelStyle={defaultLabelStyle} 
+        startAngle={90}
+        labelPosition={75}
+        
+        totalValue={sum}
+        toolTip={({dataEntry}) => `${dataEntry.title}`}
+        className='text-white scale-75 text-xs'/>
+        :
+        <p>No results</p>}
       </div>
-      <div className="grid grid-cols-1 justify-center place-content-center justify-items-center py-4">
+
+      <div className="grid grid-cols-1 justify-center place-content-center justify-items-center ">
         <div className="font-bold text-amber-600 text-2xl">Total Count: <span className='text-white'>{sum}</span></div>
       </div>
     </section>
